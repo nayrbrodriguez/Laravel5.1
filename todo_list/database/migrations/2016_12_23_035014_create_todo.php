e5783c7062b3f9f8aee0cf8495c17306e43d4a58
nayrbrodriguez <bryan@multisyscorp.com>
2016-12-23 17:03:14 +0800

    todo_list

---
+++ todo_list/database/migrations/2016_12_23_035014_create_todo.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todo', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todo', function (Blueprint $table) {
+            //
+        });
+    }
+}
